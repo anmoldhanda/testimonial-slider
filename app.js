31ad5e47969a264829fe6191e888a89d921944e5
anmol dhanda <rk352714@gmail.com>
2024-01-31 21:27:15 +0530

    responsive ui

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ let control3 = document.getElementById("control3");
 let control4 = document.getElementById("control4");
 
 control1.addEventListener("click", function () {
-    testimonials.style.transform = "translateX(950px)";
-    control1.classList.add("active");
-    control2.classList.remove("active");
-    control3.classList.remove("active");
-    control4.classList.remove("active");
-})
+  testimonials.style.transform = "translateX(950px)";
+  control1.classList.add("active");
+  control2.classList.remove("active");
+  control3.classList.remove("active");
+  control4.classList.remove("active");
+});
 
 control2.addEventListener("click", function () {
-    testimonials.style.transform = "translateX(0px)";
-    control1.classList.remove("active");
-    control2.classList.add("active");
-    control3.classList.remove("active");
-    control4.classList.remove("active");
-})
+  testimonials.style.transform = "translateX(0px)";
+  control1.classList.remove("active");
+  control2.classList.add("active");
+  control3.classList.remove("active");
+  control4.classList.remove("active");
+});
 
 control3.addEventListener("click", function () {
-    testimonials.style.transform = "translateX(-940px)";
-    control1.classList.remove("active");
-    control2.classList.remove("active");
-    control3.classList.add("active");
-    control4.classList.remove("active");
-})
+  testimonials.style.transform = "translateX(-940px)";
+  control1.classList.remove("active");
+  control2.classList.remove("active");
+  control3.classList.add("active");
+  control4.classList.remove("active");
+});
 
 control4.addEventListener("click", function () {
-    testimonials.style.transform = "translate(-1880px)";
-    control1.classList.remove("active");
-    control2.classList.remove("active");
-    control3.classList.remove("active");
-    control4.classList.add("active");
-})
+  testimonials.style.transform = "translate(-1880px)";
+  control1.classList.remove("active");
+  control2.classList.remove("active");
+  control3.classList.remove("active");
+  control4.classList.add("active");
+});
